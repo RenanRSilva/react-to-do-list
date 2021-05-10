f9c366d0caf0fb3e4c6267a4a33eba641991a6ea
Renan Silva <renanrambuls@gmail.com>
2021-05-10 14:14:18 -0300

    editar tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,23 +4,65 @@ import React, { Component } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 // Tarefas
-import { FaEdit, FaWindowClose } from 'react-icons/fa'
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: [
-      'Fazer café',
-      'Beber água',
-      'Estudar',
-    ]
+    tarefas: [],
+    index: -1,
   };
 
-  inputMudou = (e) => {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    if(index === -1){
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
+  }
+
+
+
+  handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
+    });
+  }
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  }
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
     });
   }
 
@@ -31,7 +73,7 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input
             onChange={this.handleChange}
             type="text"
@@ -46,14 +88,20 @@ export default class Main extends Component {
           {tarefas.map((tarefa) => (
             <li key={tarefa}>
               {tarefa}
-              <div>
-                <FaEdit className="edit" />
-                <FaWindowClose className="Delete" />
-              </div>
+              <span>
+                <FaEdit
+                  className="edit"
+                  onClick={(e) => this.handleEdit(e, index)}
+                />
+                <FaWindowClose
+                  className="edit"
+                  onClick={(e) => this.handleDelete(e, index)}
+                />
+              </span>
             </li>
           ))}
         </ul>
       </div>
     );
   }
-}
+
