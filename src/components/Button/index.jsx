81b4373b7c5f4982516c324415f85333e76d9444
Renan Silva <renanrambuls@gmail.com>
2021-05-26 08:59:01 -0300

    correction

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,29 @@
+import P from 'prop-types';
+import './styles.css'
+import React from 'react';
+
+import { Component } from 'react';
+export class Button extends Component {
+  render() {
+    const { text, onClick, disabled } = this.props;
+
+    return (
+      <button
+        disabled={disabled}
+        className='button'
+        onClick={onClick}>
+        {text}
+      </button>
+    )
+  }
+}
+Button.defaultProps = {
+  disabled: false,
+  
+}
+
+Button.propTypes = {
+  text: P.string.isRequired,
+  onClick: P.func.isRequired,
+  disabled: P.bool,
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './styles/global-styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './templates/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
